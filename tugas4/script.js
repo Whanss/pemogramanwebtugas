0d532afed07d9d3af81a7b1aa8a5cac287887582
Whanss <elyaivanmey@gmail.com>
2024-10-17 15:17:16 +0800

    commit4

---
+++ tugas4/script.js
@@ -0,0 +1,29 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) {
+            slide.classList.add('active');
+        }
+    });
+}
+
+function changeSlide(direction) {
+    currentSlide += direction;
+
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    } else if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+
+    showSlide(currentSlide);
+}
+  
+setInterval(() => {
+    changeSlide(1);
+}, 5000); 
+
+const menuItems = document.querySelectorAll('.menu li a');
